e2cc0d2aca7ff1efd06518e4bf72bc0de533bbdd
Michael Lai <themichaellai@gmail.com>
2016-01-31 23:52:56 -0500

    Initial commit

---
+++ app/main.js
@@ -0,0 +1,8 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+import { Router, Route, Link } from 'react-router'
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('main')
+);
