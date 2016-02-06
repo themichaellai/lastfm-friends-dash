c3053ea6d1b1fa3103c08c23ee1bfe9e4832b6c8
Michael Lai <themichaellai@gmail.com>
2016-02-06 01:08:34 -0500

    Build configuration

---
+++ README.md
@@ -0,0 +1,6 @@
+last.fm friends dashboard
+=========================
+A dashboard to show what friends are listening to.
+
+To build this, run `npm install` and `npm run deploy` or `npm run start`. The
+built app is in the `dist` folder.
--- webpack.config.js
+++ webpack.config.js
@@ -22,8 +22,13 @@ module.exports = {
     }),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.NoErrorsPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+      },
+    }),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development')
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
     })
   ],
   module: {
